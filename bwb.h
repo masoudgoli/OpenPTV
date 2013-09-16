0157130287d68a198b5d1e9d3af73eecfbc682ab
GyroGearloose <brueglju@gmail.com>
2013-09-16 18:15:30 +0200

    Create bwb.h

---
+++ bwb.h
@@ -0,0 +1,31 @@
+class Board2Board
+{
+    
+
+};
+
+
+/*
+P-Funktion
+car stopped -> press gas pedal
+large error, accelerates
+too much gas : gain too high
+
+D-Function
+control position (to next traffic light)
+far away, error is big,
+driving, overshoot, back up
+
+PD: rate of change and magnitude
+derivative is smaller
+
+I: same speed behind car: we have a steady state error.
+I tries to close up
+
+proportional trys to get you as fast as possible 
+derivative trys to restrain you to move to quickly
+
+
+
+
+*/
